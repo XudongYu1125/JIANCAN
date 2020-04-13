8790e3097d13156dcd45500cd0e2ba174e9ac518
yuxudonglinziyi <1479121080@qq.com>
2020-04-13 09:25:40 +0800

    all

---
+++ 客户端/JIANCAN/app/src/main/java/com/example/user/jiancan/HomeListItemBean.java
@@ -0,0 +1,11 @@
+package com.example.user.jiancan;
+
+/**
+ * 首页列表item实体bean
+ */
+public class HomeListItemBean {
+    public String name;
+    public String user;
+    public String likeNum;
+    public int showImg;
+}
