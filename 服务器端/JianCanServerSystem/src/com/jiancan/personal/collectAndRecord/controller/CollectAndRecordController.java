f06b89aa2f8d00e6e47efa4a129ae4c5e1cd3ee5
Gaoyunfei429 <491966191@qq.com>
2020-04-16 12:34:25 +0800

    .

--- 服务器端/JianCanServerSystem/src/com/jiancan/personal/collectAndRecord/controller/CollectAndRecordController.java
+++ 服务器端/JianCanServerSystem/src/com/jiancan/personal/collectAndRecord/controller/CollectAndRecordController.java
@@ -1,8 +1,52 @@
 package com.jiancan.personal.collectAndRecord.controller;
 
+import java.util.List;
+
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.gson.Gson;
+import com.jiancan.personal.collectAndRecord.service.CollectAndRecordService;
 
 @Controller
+@ResponseBody
+@RequestMapping("/car")
 public class CollectAndRecordController {
-
+	@Resource
+	CollectAndRecordService collectAndRecordService;
+	//通过userId和foodId添加收藏
+	@RequestMapping(value = "/ac/{userId}/{foodId}",method = RequestMethod.GET)
+	public String addCollect(@PathVariable int userId,@PathVariable int foodId) {		
+		return new Gson().toJson(collectAndRecordService.add(userId, foodId, 0));
+	}
+	//通过userId和foodId删除收藏
+	@RequestMapping(value = "/ac/{userId}/{foodId}",method = RequestMethod.GET)
+	public String removeCollect(@PathVariable int userId,@PathVariable int foodId) {
+		return new Gson().toJson(collectAndRecordService.remove(userId, foodId, 0));
+	}
+	//通过userId查询收藏
+	@RequestMapping(value = "/ac/{userId}",method = RequestMethod.GET)
+	public String findCollectFoods(@PathVariable int userId) {
+		return new Gson().toJson(collectAndRecordService.findFoods(userId, 0));
+	}
+	//通过userId和foodId添加历史记录
+	@RequestMapping(value = "/ar/{userId}/{foodId}",method = RequestMethod.GET)
+	public String addRecord(@PathVariable int userId,@PathVariable int foodId) {		
+		return new Gson().toJson(collectAndRecordService.add(userId, foodId, 1));
+	}
+	//通过userId和foodId删除历史记录
+	@RequestMapping(value = "/ar/{userId}/{foodId}",method = RequestMethod.GET)
+	public String removeRecord(@PathVariable int userId,@PathVariable int foodId) {
+		return new Gson().toJson(collectAndRecordService.remove(userId, foodId, 1));
+	}
+	//通过userId查询历史记录
+	@RequestMapping(value = "/ar/{userId}",method = RequestMethod.GET)
+	public String findRecordFoods(@PathVariable int userId) {
+		return new Gson().toJson(collectAndRecordService.findFoods(userId, 1));
+	}
 }
--- 服务器端/JianCanServerSystem/src/com/jiancan/personal/collectAndRecord/dao/CollectAndRecordDao.java
+++ 服务器端/JianCanServerSystem/src/com/jiancan/personal/collectAndRecord/dao/CollectAndRecordDao.java
@@ -1,8 +1,63 @@
 package com.jiancan.personal.collectAndRecord.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Repository;
+
+import com.jiancan.entity.personal.TUser;
+import com.jiancan.entity.personal.User;
+import com.jiancan.entity.vegetables.Food;
 
 @Repository
 public class CollectAndRecordDao {
-
+	@Resource
+	private SessionFactory sessionFactory;
+	private Session session = this.sessionFactory.getCurrentSession();
+	private Transaction txTransaction = session.beginTransaction();
+	//userId和foodId添加记录
+	public int insert(int userId,int foodId,int type) {
+		String str="insert into collectandrecord(userId,foodId,type) value(?,?,?)";
+		Query query = session.createQuery(str);
+		query.setParameter(0, userId);
+		query.setParameter(1, foodId);
+		query.setParameter(2, type);
+		int i = query.executeUpdate();
+		txTransaction.commit();
+		return i;
+		
+	}
+	//userId和foodId删除记录
+	public int delete(int userId,int foodId,int type) {
+		String str="delete from collectandrecord where userId=? and foodId=? and type=?";
+		Query query = session.createQuery(str);
+		query.setParameter(0, userId);
+		query.setParameter(1, foodId);
+		query.setParameter(2, type);
+		int i = query.executeUpdate();
+		txTransaction.commit();
+		return i;
+	}
+	//根据foodIdList查询记录foodList
+	public List<Food> selectRecordsByList(List foodIdList) {
+		String sql="from food where id in ?";
+		Query query = this.session.createQuery(sql);
+		query.setParameter(0, foodIdList);
+		return (List<Food>)query.list();	
+	}
+	//根据foodIdList查询记录foodList
+	public List<Food> selectRecordsByUserId(int userId,int type) {
+		String sql="select foodId from collectandrecord where uerId=? and type=?";
+		Query query = this.session.createQuery(sql);
+		query.setParameter(0, userId);
+		query.setParameter(1, type);
+		return selectRecordsByList(query.list());
+	}
 }
