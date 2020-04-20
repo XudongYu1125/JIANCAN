bed4d1febcd1da536c7dd627d2b3536ec6e9307f
xiaozhiqiang708 <944428625@qq.com>
2020-04-20 21:14:30 +0800

    upload

--- 服务器端/JianCanServerSystem/src/com/jiancan/vegetables/comment/controller/CommentController.java
+++ 服务器端/JianCanServerSystem/src/com/jiancan/vegetables/comment/controller/CommentController.java
@@ -7,7 +7,6 @@ import java.io.UnsupportedEncodingException;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.validation.Valid;
 
 import org.json.JSONObject;
 import org.springframework.http.converter.json.GsonFactoryBean;
