2761c06a478ec6984eab29dd5a1043283145cce0
xiaozhiqiang708 <944428625@qq.com>
2020-04-20 21:22:36 +0800

    upload

---
+++ 服务器端/JianCanServerSystem/src/com/jiancan/entity/personal/TUser.java
@@ -0,0 +1,26 @@
+package com.jiancan.entity.personal;
+
+public class TUser {
+	private int id;
+	private String nickname;
+	private String imageUrl;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getImageUrl() {
+		return imageUrl;
+	}
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+	
+}
