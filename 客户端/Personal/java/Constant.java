90f2d9a1d883817610f098a00cf73ee50f87570c
tianying10aha <2732754328@qq.com>
2020-04-22 16:33:04 +0800

    .

--- 客户端/Personal/java/Constant.java
+++ 客户端/Personal/java/Constant.java
@@ -1,8 +1,11 @@
 package com.example.user.jiancan;
 
 public class Constant {
-    public static final String BASE_IP = "http://10.7.88.67:8080/";
+    public static final String BASE_IP = "";
     public static final String BASE_URL= BASE_IP + "";
     public static final String URL_UPLOAD_PIC = BASE_URL + "";
     public static final String URL_CHANGE_NAME = BASE_URL + "";
+    public static final String URL_TRENDS = BASE_URL + "";
+    public static final String URL_FOLLOW = BASE_URL + "";
+    public static final String URL_FAN = BASE_URL + "";
 }
