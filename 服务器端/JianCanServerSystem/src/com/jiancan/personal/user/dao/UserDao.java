0c69f5b7a170618c681cd4ff14d5afc8bd1b7b02
Gaoyunfei429 <1293873211@qq.com>
2020-04-23 13:46:25 +0800

    .

--- 服务器端/JianCanServerSystem/src/com/jiancan/personal/user/dao/UserDao.java
+++ 服务器端/JianCanServerSystem/src/com/jiancan/personal/user/dao/UserDao.java
@@ -1,8 +1,82 @@
 package com.jiancan.personal.user.dao;
 
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Repository;
+
+import com.jiancan.entity.personal.User;
 
 @Repository
 public class UserDao {
-
+	@Resource
+	private SessionFactory sessionFactory;
+	private Session session = this.sessionFactory.getCurrentSession();
+	private Transaction txTransaction = session.beginTransaction();
+	public List<User> selectAllUsers(){
+		String sql = "from User";
+		Query query = session.createQuery(sql);
+		return (List<User>)query.list();
+	}
+	public int insertUser(User user) {
+		String sql= "insert into User (sex,nickname,phone,password,imageurl) value(?,?,?,?,?)";
+		Query query = session.createQuery(sql);
+		query.setParameter(0, user.getSex());
+		query.setParameter(1, user.getNickname());
+		query.setParameter(2, user.getPhone());
+		query.setParameter(3, user.getPassword());
+		query.setParameter(4, user.getImageUrl());
+		return query.executeUpdate();
+	}
+	public int deleteUser(int id) {
+		String sql= "delete from User where id = ?";
+		Query query = session.createQuery(sql);
+		query.setParameter(0, id);
+		return query.executeUpdate();
+	}
+	public int updateUser(User user) {
+		String sql= "update User set sex=?,nickname=?,phone=?,password=?,imageurl=?";
+		Query query = session.createQuery(sql);
+		query.setParameter(0, user.getSex());
+		query.setParameter(1, user.getNickname());
+		query.setParameter(2, user.getPhone());
+		query.setParameter(3, user.getPassword());
+		query.setParameter(4, user.getImageUrl());
+		return query.executeUpdate();
+	}
+	public User selectUserById(int id) {
+		String sql = "from User where id=?";
+		Query query = session.createQuery(sql);
+		query.setParameter(0, id);
+		List<User>users = (List<User>)query.list();
+		if(users==null) {
+			return users.get(0);
+		}
+		return null;
+	}
+	public User selectUserByNickname(String nickname) {
+		String sql = "from User where nickname=?";
+		Query query = session.createQuery(sql);
+		query.setParameter(0, nickname);
+		List<User>users = (List<User>)query.list();
+		if(users==null) {
+			return users.get(0);
+		}
+		return null;
+	}
+	public User selectUserByPhone(String phone) {
+		String sql = "from User where phone=?";
+		Query query = session.createQuery(sql);
+		query.setParameter(0, phone);
+		List<User>users = (List<User>)query.list();
+		if(users==null) {
+			return users.get(0);
+		}
+		return null;
+	}
 }
--- 服务器端/JianCanServerSystem/src/com/jiancan/personal/user/service/UserService.java
+++ 服务器端/JianCanServerSystem/src/com/jiancan/personal/user/service/UserService.java
@@ -1,8 +1,56 @@
 package com.jiancan.personal.user.service;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Service;
+
+import com.google.gson.Gson;
+import com.jiancan.entity.personal.User;
+import com.jiancan.personal.user.dao.UserDao;
 
 @Service
 public class UserService {
-
+	@Resource
+	private UserDao userDao;
+	//注册用户
+	//返回0代表注册失败
+	public String addUser(String userJson) {
+		User user = new Gson().fromJson(userJson, User.class);
+		return userDao.insertUser(user)+"";
+	}
+	//注销用户
+	//返回0代表注销失败
+	public String removeUser(String id) {
+		return userDao.deleteUser(Integer.parseInt(id))+"";
+	}
+	//修改用户信息
+	//返回0代表修改失败
+	public String editUser(String userJson) {
+		User user = new Gson().fromJson(userJson, User.class);
+		return userDao.updateUser(user)+"";
+	}
+	//通过昵称登录
+	//返回值0代表用户不存在，1代表密码错误
+	public String loginByNickname(String nickname,String password) {
+		User user = userDao.selectUserByNickname(nickname);
+		if(user==null) {
+			return "0";
+		}else if(user.getPassword()==password) {
+			return new Gson().toJson(user);
+		}else {
+			return "1";
+		}
+	}
+	//通过手机号登录
+	//返回值0代表用户不存在，1代表密码错误
+	public String loginByPhone(String phone,String password) {
+		User user = userDao.selectUserByPhone(phone);
+		if(user==null) {
+			return "0";
+		}else if(user.getPassword()==password) {
+			return new Gson().toJson(user);
+		}else {
+			return "1";
+		}
+	}
 }
